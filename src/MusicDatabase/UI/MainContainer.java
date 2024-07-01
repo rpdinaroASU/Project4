c50a8190ab315b78571ea9fa8122bbe870dfaecd
rpdinaroASU <133998885+rpdinaroASU@users.noreply.github.com>
2024-06-30 21:47:15 -0600

    Add Javadocs to Main Container

--- src/MusicDatabase/UI/MainContainer.java
+++ src/MusicDatabase/UI/MainContainer.java
@@ -4,12 +4,25 @@ package MusicDatabase.UI;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class is a Singleton of the UI, this ensures that only one instance of the UI can be called at once.
+ * The Main Container is composed of three distinct sections, the Menu to the left which allows
+ * a User to select the context of their interaction with the database, the ContextPane in the center that
+ * displays ResultSets according to user specification, Options above the Context pane which will
+ * allow the user to do common actions, such as adding, deleting, updating, and applying filters.
+ *
+ * @author Ryan Dinaro
+ * @version 1.0.0
+ */
 public class MainContainer {
     private static MainContainer mainContainer;
 
-    private final JFrame frame;
+    /**
+     * This method initializes the Singleton MainContainer, adding the three sections to the
+     * container.
+     */
     private MainContainer() {
-        frame = new JFrame();
+        JFrame frame = new JFrame();
 
         frame.setVisible(true);
         frame.setName("Music Database");
@@ -27,6 +40,10 @@ public class MainContainer {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
+    /**
+     * Singleton instance of the MainContainer
+     * @return Returns the singleton instance
+     */
     public static MainContainer getInstance() {
         if(mainContainer==null) {
             mainContainer = new MainContainer();
