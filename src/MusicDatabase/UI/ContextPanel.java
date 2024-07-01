82908efd878ad409a9df37e898be630d520e99ea
rpdinaroASU <133998885+rpdinaroASU@users.noreply.github.com>
2024-06-30 23:33:05 -0600

    Add Javadocs to context panel

--- src/MusicDatabase/UI/ContextPanel.java
+++ src/MusicDatabase/UI/ContextPanel.java
@@ -1,16 +1,29 @@
 package MusicDatabase.UI;
 
 import javax.swing.*;
-import java.awt.*;
 
+/**
+ * This class is the context panel, the displayed values of the context panel are the returns of
+ * the JDBC
+ * @author Ryan Dinaro
+ * @version 1.0.0
+ */
 public class ContextPanel {
     private static ContextPanel contextPanelInstance;
     private final JPanel contextPanel;
+
+    /**
+     * Singleton of the context panel
+     */
     private ContextPanel() {
         contextPanel = new JPanel();
         //TODO: CREATE CONTEXT PANEL
     }
 
+    /**
+     * Initializes and returns the singleton instance of the context panel
+     * @return singleton instance of the context panel
+     */
     public static ContextPanel getInstance() {
         if(contextPanelInstance==null) {
             contextPanelInstance = new ContextPanel();
@@ -19,6 +32,10 @@ public class ContextPanel {
         return contextPanelInstance;
     }
 
+    /**
+     * returns the contextPanel JPanel
+     * @return JPanel containing the contextPanel
+     */
     public JPanel getContextPanel() {
         return contextPanel;
     }
