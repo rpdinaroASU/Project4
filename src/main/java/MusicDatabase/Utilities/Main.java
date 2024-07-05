e66d6783faab024150d3ce171b3f82f41572698d
Zoe Shortman <147352710+zshortma@users.noreply.github.com>
2024-07-05 14:18:49 -0500

    Adding login configuration in build.gradle. (#6)

--- src/main/java/MusicDatabase/Utilities/Main.java
+++ src/main/java/MusicDatabase/Utilities/Main.java
@@ -1,11 +1,18 @@
 package MusicDatabase.Utilities;
 
 import MusicDatabase.UI.MainContainer;
+import MusicDatabase.JDBC.MusicDatabaseConnector;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
+    	
+    	 System.out.println("Database URL: " + System.getProperty("db.url"));
+         System.out.println("Database Username: " + System.getProperty("db.username"));
+         System.out.println("Database Password: " + System.getProperty("db.password"));
+         
         MainContainer.getInstance();
     }
 }
